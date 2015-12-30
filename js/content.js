02024c1bac9024fb9a366fe87486eeb352f74702
ChaseTerzian <chaseterzian@gmail.com>
2015-12-29 18:38:19 -0700

    style additions on all pages

---
+++ js/content.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	$('#code-projects-page').hide();
+	$('#code-projects-page').fadeIn(500);
+
+	$('#gearbox-sect').hide();
+	$('#gearbox-sect').fadeIn(500);
+
+	$('#the-projects-sect').hide();
+	$('#the-projects-sect').fadeIn(500);
+
+	$('#whiteboard-sect').hide();
+	$('#whiteboard-sect').fadeIn(500);
+
+	$('#music-page-section').hide();
+	$('#music-page-section').fadeIn(500);
+
+	$('#media-page-section').hide();
+	$('#media-page-section').fadeIn(500);
+
+	$('#other-resources-page-section').hide();
+	$('#other-resources-page-section').fadeIn(500);
+
+	$('#contact-page-section').hide();
+	$('#contact-page-section').fadeIn(500);
+
+});
--- js/style.js
+++ js/style.js
@@ -6,20 +6,15 @@ $(document).ready(function() {
 	$('#landing-link-buttons').hide();
 	$('#landing-site-buttons').hide();
 	$('hr').hide();
-
 	// $('.bg1').fadeIn(1000);
 	$('#chase-terzian').fadeIn(1000);
-	
-
 	setTimeout(function() {
 		$('#the-source').fadeIn(1000);
 	},200);
-
 	setTimeout(function() {
 	$('#landing-link-buttons').fadeIn(1000);
 	$('hr').fadeIn(3000);
 	},400);
-
 	setTimeout(function() {
 	$('#landing-site-buttons').fadeIn(1000);
 	},600); 
